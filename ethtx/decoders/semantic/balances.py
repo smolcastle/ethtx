9168a5057536891d96ffbc3f163d003efb02c43e
Smol Ninja <smolninja@protonmail.com>
2022-03-06 19:56:37 +0000

    remove unused import

--- ethtx/decoders/semantic/balances.py
+++ ethtx/decoders/semantic/balances.py
@@ -10,7 +10,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from inspect import Parameter
 from typing import List
 
 from ethtx.models.decoded_model import DecodedBalance, DecodedTransactionMetadata, DecodedEvent, AddressInfo
